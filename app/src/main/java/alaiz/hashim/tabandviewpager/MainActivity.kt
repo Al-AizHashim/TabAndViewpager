183c3716d814823f59b4930dac92eb8612e17b79
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-11-04 00:46:27 +0300

    show the current number of page that is displayed

--- app/src/main/java/alaiz/hashim/tabandviewpager/MainActivity.kt
+++ app/src/main/java/alaiz/hashim/tabandviewpager/MainActivity.kt
@@ -2,6 +2,7 @@ package alaiz.hashim.tabandviewpager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -25,6 +26,20 @@ class MainActivity : AppCompatActivity() {
                 tabLayout.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_home_24)
                 tabLayout.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_audiotrack_24)
                 tabLayout.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_favorite_24)
+                tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+                    override fun onTabSelected(tab: TabLayout.Tab?) {
+                         Toast.makeText(this@MainActivity , "the number of current page is: "+tab?.position.toString(), Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onTabReselected(tab: TabLayout.Tab?) {
+                        // Handle tab reselect
+                    }
+
+                    override fun onTabUnselected(tab: TabLayout.Tab?) {
+                        // Handle tab unselect
+                    }
+                })
 
                return when (position) {
                     0 -> {
@@ -45,6 +60,7 @@ class MainActivity : AppCompatActivity() {
             override fun getItemCount(): Int {
                 return 3
             }
+
         }
         TabLayoutMediator(tabLayout, tabViewPager){tab,position->
             tab.text=when(position){
