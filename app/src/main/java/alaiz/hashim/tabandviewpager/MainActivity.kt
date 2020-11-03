bafa7f372fba3106ea89512ce30861011d87c28b
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-11-04 00:24:23 +0300

    adding icons

--- app/src/main/java/alaiz/hashim/tabandviewpager/MainActivity.kt
+++ app/src/main/java/alaiz/hashim/tabandviewpager/MainActivity.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabItem
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 lateinit var tabLayout:TabLayout
 lateinit var tabViewPager:ViewPager2
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tabLayout=findViewById(R.id.tabs)
         tabViewPager=findViewById(R.id.pager)
+
+
         tabViewPager.adapter=object :FragmentStateAdapter(this) {
             override fun createFragment(position: Int): Fragment {
-
+                tabLayout.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_home_24)
+                tabLayout.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_audiotrack_24)
+                tabLayout.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_favorite_24)
 
                return when (position) {
                     0 -> {
